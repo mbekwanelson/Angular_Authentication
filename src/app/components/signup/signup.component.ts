3d9b25006e17ddb6cd85922c69e6319b20e5b2a3
Ntsikelelo.Mbekwa <mbekwantsikelelonelson@gmail.com>
2022-09-25 00:42:44 +0200

    1.Create New Project in Angular and .NET Core - done,   vs studio, create web api .net core, .net 6.0 2. Create Signup + login page: 	 Create components, login,signup, add common style.css in assets/common and import in style.css 	 Routes on app.route-module, 	 add router-outlet tag on app.component.html 	 add bootstrap cdn links on index.html 	 add fontawesome cdn link on index.html 	 h3.pt-3.font-weight-bold then <tab> quickly generate html tags with classes 	 Front-end logic : toggle Password visibility

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  togglePasswordVisibility(){
+
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+}
